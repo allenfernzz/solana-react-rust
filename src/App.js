4e725803d91e80ea0d1b54bfc01c77a21660448b
allenfernzz <allen.fdes@gmail.com>
2022-09-28 23:17:55 +0530

    connect wallet button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -6,11 +6,16 @@ import './App.css';
 const TWITTER_HANDLE = 'allenfernzz';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+// Abc123$#
+
 const App = () => {
    /*
    * This function holds the logic for deciding if a Phantom Wallet is
    * connected or not
    */
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -27,6 +32,10 @@ const App = () => {
             'Connected with Public Key:',
             response.publicKey.toString()
           );
+          /*
+           * Set the user's publicKey in state to be used later!
+           */
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
@@ -40,7 +49,15 @@ const App = () => {
    * Let's define this method so our code doesn't break.
    * We will write the logic for this next!
    */
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button" onClick={connectWallet}>
@@ -62,14 +79,15 @@ const App = () => {
   
   return (
     <div className="App">
-      <div className="container">
+  
+       <div className={ walletAddress ? 'authed-container' : 'container' }>
         <div className="header-container">
           <p className="header">Web3 Notes</p>
           <p className="sub-text">
             View your notes stored on the blockchain ✨
           </p>
-          {/* Render your connect to wallet button right here */}
-          {renderNotConnectedContainer()}
+          {/* Add the condition to show this only if we don't have a wallet address */}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
